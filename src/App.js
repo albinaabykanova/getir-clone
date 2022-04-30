70791b9f9965025a0e15a5e3591b0929edb58097
Albina Abykanova <abykanovaalbina@gmail.com>
2022-05-01 01:18:10 +0300

    Mobile design change

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import Favorites from  "./components/Favorites";
 import MobileApp from  "./components/MobileApp";
 import Cards from  "./components/Cards";
 import Footer from  "./components/Footer";
+import { useWindowWidth } from '@react-hook/window-size';
 
 function App() {
+  const windowWidth = useWindowWidth()
+
   return (
     <>
-       <Header />
-       <HeroSection />
-       <Categories />
-       <Campaingns />
-       <div className="container mx-auto grid gap-y-6">
-          <Favorites />
-          <MobileApp />
-          <Cards />
+        <Header />
+        {windowWidth < 768 && <Campaingns />} 
+        <HeroSection />
+        <Categories />
+        <Campaingns />
+        
+       
+        <div className="container mx-auto grid gap-y-6 pt-8">
+            <Favorites />
+            <MobileApp />
+            <Cards />
        </div>
        <Footer />
 
